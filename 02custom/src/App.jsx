18f4f1ed01f6f532fc2a91bae57b2821b10b73d0
Tanvijikar17 <tanvijikar@gmail.com>
2024-12-04 23:43:43 +0530

    New projects for hooks

---
+++ 02custom/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+let counter = 15
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello this is custom react</h1>
+      <h2>We are goint to learn hooks</h2>
+      <button Add value >Add value</button> 
+      <br /> <br /> 
+      <button>Remove value</button>
+    </>
+  )
+}
+
+export default App
