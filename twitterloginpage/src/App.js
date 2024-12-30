f55b51e965fef6201ae77bf43ac58e7218e008ff
Tanvijikar17 <tanvijikar@gmail.com>
2024-12-30 13:03:53 +0530

    New page

---
+++ twitterloginpage/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return(
+        <div classname="App">
+            <div classname="logo-box">
+                
+            </div>
+        </div>
+    )
+}
+export default App;
+
