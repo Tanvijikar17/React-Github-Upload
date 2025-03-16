ae37d7ad6fd3804b2255506fd2418d9703bf7b95
Tanvijikar17 <tanvijikar@gmail.com>
2025-03-16 22:58:28 +0530

    new

--- bmi_cal/src/App.js
+++ bmi_cal/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <h1>hellow world</h1>
     <h1>This is a BMI Calculator</h1>
     <h1>It will be made soon</h1>
+    <h1>It will be made soon</h1>
     <h1>Today is my birthday !! remember to be happy !</h1>
     </div>
   );
