f369c3d340cad7e814a9b162efca650bd64ef751
Tanvijikar17 <tanvijikar@gmail.com>
2025-03-20 23:45:50 +0530

    calulator start

--- bmi_cal/src/App.js
+++ bmi_cal/src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>hellow world</h1>
+      <div className = "conntainer">
+        <h2>BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight (ibs)</label>
+            <input type="text"  placeholder="Enter your weight" value={weight}/>
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input type="text" placeholder="Enter your weight" value={height}/>
+          </div>
+          <div>
+            <button className= 'btn' type='submit'>Submit</button>
+            <button className= 'btn btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+
+          <div className='center'> </div>
+        </form>
+      </div>
+
+
+
+
     </div>
   );
 }
